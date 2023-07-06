879b4f30658a9e251fc3ac7c57ca163a2fc44100
giu08 <88555889+giu08@users.noreply.github.com>
2023-07-06 11:19:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-fetch("lists.json")
-    .then(Response => Response.json)
-    .then(data => {
-        console.log(data)
-    })
+import json from './lists.json' assert {type: 'json'}
 
-function randomRange(min, max) {
-    return Math.round(min + (Math.random()*(max-min)))
+console.log(json)
+
+function random(n1, n2) {
+    let output = Math.floor(n1 + (Math.random() * (n2-(n1-1))))
+    return output
 }
 
-function makeWord() {
-    subject = json.subject[randomRange(0, json.subjects.length)]
-    insult = json.insults[randomRange(0, json.insults.length)]
-    word = function(s, i) {
+console.log(json.subjects[random(0, json.subjects.length -1)])
+
+let makeWord = function() {
+    let iS = random(0, json.subjects.length -1)
+    let iI = random(0, json.insults.length -1)
+    let s = json.subjects[iS]
+    let i = json.insults[iI]
+
+        let ins = null
+        let order = null
+        let output = null
+
         if (s.gender == "m") {
             ins = i.mWord
         } else if (s.gender = "f") {
             ins = i.fWord
         }
-        sub = s.name
-        if (i.type = 2) {
-            order = randomRange(0, 1)
+        let sub = s.name
+        
+        if (i.type == 2) {
+            order = random(0, 1)
         } else {
             order = i.type
         }
+        console.log([order, json.insults[1].type, i, iI])
         if (order == 0) {
-            output = [ins, sub].join
+            output = [ins, sub].join(' ')
         } else if (order == 1) {
-            output = [sub, ins].join
+            output = [sub, ins].join(' ')
         }
          
-        return string(output)
-    }
-    return word
+        return String(output)
 }
+
+
 
 document.getElementById("1").onclick = function() {
     document.getElementById("2").innerText = makeWord()
