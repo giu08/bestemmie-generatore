317aadb2f19cc116505f3c312f5933b55fe84839
loop <88555889+giu08@users.noreply.github.com>
2023-07-07 12:35:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,35 +14,40 @@ let makeWord = function() {
     let iI = random(0, object.insults.length -1)
     let s = object.subjects[iS]
     let i = object.insults[iI]
-        let ins = null
-        let order = null
-        let output = null
+    let ins = null
+    let order = null
+    let output = null
 
-        if (s.gender == "m") {
-            ins = i.mWord
-        } else if (s.gender = "f") {
-            ins = i.fWord
-        }
-        let sub = s.name
-        
-        if (i.type == 2) {
-            order = random(0, 1)
-        } else {
-            order = i.type
-        }
-        if (order == 0) {
+    if (s.gender == "m") {
+        ins = i.mWord
+    } else if (s.gender = "f") {
+        ins = i.fWord
+    }
+    let sub = s.name
+    
+    if (i.type == 2) {
+        order = random(0, 1)
+    } else {
+        order = i.type
+    }
+    switch (order) {
+        case 0:
             output = [ins, sub].join(' ')
-        } else if (order == 1) {
+            break;
+        case 1:
             output = [sub, ins].join(' ')
-        } else if (order == 3) {
+            break;
+        case 3:
             if (ins.includes("<sub>")) {
                 output = ins.replace('<sub>', sub)
             } else {
                 output = ins
             }
-        }
-         
-        return String(output)
+            break;
+    }
+    
+    
+    return String(output)
 }
 
 
